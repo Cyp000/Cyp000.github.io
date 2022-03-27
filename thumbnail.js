046a5a4fd97c7cb3fe56d48093fdcfd3d9c83ed9
Cyp000 <65027591+Cyp000@users.noreply.github.com>
2022-03-27 23:20:15 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-});
-window.onload=function(){
-let targetImage = document.querySelector("#smart-image");
   
-targetImage.addEventListener("click", function() {
-if (targetImage.classList.contains("small")) {
-targetImage.classList.remove("small");
-} else {
-targetImage.classList.add("small");
-}
+  
+    let targetImage = document.querySelector("#smart-image");
+     targetImage.addEventListener("click", function() {
+     if (targetImage.classList.contains("small")) {
+     targetImage.classList.remove("small");
+     } else {
+     targetImage.classList.add("small");
+ }
 });
-}
+})
