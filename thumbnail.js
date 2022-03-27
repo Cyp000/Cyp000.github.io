5a7bd1dd31cd700eb11427d6fe64e97eedc66757
Cyp000 <65027591+Cyp000@users.noreply.github.com>
2022-03-27 23:26:38 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  
-  
-})
+});
 
 let targetImage = document.querySelector("#smart-image");
 targetImage.addEventListener("click", function() {
@@ -12,3 +10,4 @@ else {
 targetImage.classList.add("small");
 }
 });
+
